67523f5026b7f21866cef97a57ab7cb85bf198c0
Ben Jay <fakebenjay@gmail.com>
2017-01-24 07:15:24 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,22 @@
+array = [1, 2, 3, 4]
+
 def my_select(collection)
- # your code here!
+  if block_given?
+    new_collection = []
+    i = 0
+    while i < collection.length
+        truthiness = (yield collection[i])
+      if truthiness == true
+        new_collection << collection[i]
+      end
+        i += 1
+    end
+    return new_collection
+  else
+    return "Oops!"
+  end
+end
+
+my_select(array) do |num|
+  num.even?
 end
